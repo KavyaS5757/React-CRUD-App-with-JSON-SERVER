c8f1cb10e38d8b6cb08c1f491e20f6539c478da8
Kavya <kavyasmallika@gmail.com>
2022-09-27 20:11:48 +0530

    first commit

---
+++ crud-front/src/crud/loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Spin, Space } from "antd";
+
+const loading =() =>(
+    <Space size="middle">
+        <Spin size="large" />
+    </Space>
+);
+export default loading;
